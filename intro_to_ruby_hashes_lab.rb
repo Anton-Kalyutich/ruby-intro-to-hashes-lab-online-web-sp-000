71c1316fe9e5aaed5cc3225aa80ab38bb30dfb4a
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-25 03:50:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,14 +17,6 @@ def monopoly_with_second_tier
     names: {},
     rent_in_dollars = {}
   }}
-  
-  monopoly = {}
-  monopoly[:railroads] = {}
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:names] = {}
-  monopoly[:railroads][:rent_in_dollars] = {}
-  
-  monopoly
 end
 
 def monopoly_with_third_tier
